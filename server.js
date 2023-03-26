2eac31beeaf21b410bd9be75e133501d704db7e7
yana12 <yana@rajamobil.net>
2023-03-26 22:10:46 +0700

    change server.js

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
-app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
-const port = process.env.PORT || 3000;
+const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+const compress = require('compression');
+
+const port = process.env.PORT || 9002;
+const app = express();
+
+app.use(compress());
+app.use(helmet());
+app.use(express.static(path.join(__dirname, 'dist'), { maxAge: 10 }));
+
+app.get('*', function (request, response) {
+  response.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 app.listen(port);
+console.log("server started on port " + port);
